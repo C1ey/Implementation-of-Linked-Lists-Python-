455f255ca939af5703b37262ec0a75f0ab73b949
C1ey <105993415+C1ey@users.noreply.github.com>
2024-01-04 08:34:34 -0800

    Update Linked_List.py

--- Linked_List.py
+++ Linked_List.py
@@ -86,14 +86,17 @@ class LinkedList:
     def print_prev(self, index):
         if index < 0 or index >= self.get_length():
             raise Exception ("Index Out Of Range")
-        count = self.get_length()
-        llstr=''
+        if index==0:
+            print("No previous node located")
+            return
+
+        count = 0
         itr = self.head
         while itr:
             if count== index-1:
-                llstr+=str(itr.data)+'<-- Previous'
-                count -=1
-        print(llstr)
+                print(str(itr.data)+'<-- Previous')
+            count +=1
+            itr= itr.next
         
 #This function inserst data at a specified index in the linked list
     def insert_at(self, index, data):
@@ -126,6 +129,6 @@ if __name__ == '__main__':
     link.insert_values_at_begining([1,2,3,4,5,6,7,8,9,9,9])
 ##    link.remove_at(5)
 ##    link.insert_at(5,9000)
-    #link.print_prev(2)
+    link.print_prev(2)
     link.print()
     print("length: ",link.get_length())
